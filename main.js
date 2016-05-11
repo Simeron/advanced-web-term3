1700fb7036607ca6a53fedf50c6b5f0f0c587d11
Simeron Taak <simeron03@icloud.com>
2016-05-11 19:21:43 +0100

    Have been trying to get the hide and show to work. I have found some code which allows it to work. I have moved it around the HTML to see where it would work, after I tested that the code works. I have now put it in the first div, and taken the button off it so now it is just text, I will try and manipulate it further to get it to do what I want.

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-  $(".one-half.column6").click(function(){
-   $(".one-half.column6").hide();
-   $("#test2").show();
+//   $(".one-half.column6").click(function(){
+//    $(".one-half.column6").hide();
+//    $("#test2").show();
 
+// });
+
+$(document).ready(function(){
+    $("#test").click(function(){
+        $("p").hide();
+    });
+    $("#test2").click(function(){
+        $("p").show();
+    });
 });
