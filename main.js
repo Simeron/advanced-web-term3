bf028572f77063f2c0d3caa14b7ff6d7e89c8c71
Simeron Taak <simeron03@icloud.com>
2016-05-17 16:55:40 +0100

    All of the dots now work now that Harry has fixed it.

--- main.js
+++ main.js
@@ -22,3 +22,51 @@ function hideMeTwo() {
     document.getElementById("viewtwo").style.display = "none";
     document.getElementById("lightboxone").style.display = "none";
 };
+
+function showMeThree() {
+    document.getElementById("viewthree").style.display = "block";
+    document.getElementById("lightboxone").style.display = "block";
+    document.getElementById("dots3").style.display = "block";
+};
+
+function hideMeThree() {
+    document.getElementById("viewthree").style.display = "none";
+    document.getElementById("lightboxone").style.display = "none";
+};
+
+function showMeFour() {
+    document.getElementById("viewfour").style.display = "block";
+    document.getElementById("lightboxone").style.display = "block";
+    document.getElementById("dots4").style.display = "block";
+};
+
+function hideMeFour() {
+    document.getElementById("viewfour").style.display = "none";
+    document.getElementById("lightboxone").style.display = "none";
+};
+
+
+function showMeSix() {
+    document.getElementById("viewsix").style.display = "block";
+    document.getElementById("lightboxone").style.display = "block";
+    document.getElementById("dots6").style.display = "block";
+};
+
+function hideMeSix() {
+    document.getElementById("viewsix").style.display = "none";
+    document.getElementById("lightboxone").style.display = "none";
+};
+
+
+
+function showMeSeven() {
+    document.getElementById("viewseven").style.display = "block";
+    document.getElementById("lightboxone").style.display = "block";
+};
+
+function hideMeSeven() {
+    document.getElementById("viewseven").style.display = "none";
+    document.getElementById("lightboxone").style.display = "none";
+};
+
+
