cbf3732a31daa13ffa813d6cab0c6fd4e403aa44
Simeron Taak <simeron03@icloud.com>
2016-05-19 17:19:39 +0100

    Everything works now, and now the clouds have now changed as well. EVERYTHING

--- main.js
+++ main.js
@@ -49,13 +49,39 @@ function hideMeFour() {
 function showMeFive() {
     document.getElementById("viewfive").style.display = "block";
     document.getElementById("lightboxone").style.display = "block";
-    document.getElementById("dots4").style.display = "block";
+    document.getElementById("dots5").style.display = "block";
 };
 
 function hideMeFive() {
     document.getElementById("viewfive").style.display = "none";
     document.getElementById("lightboxone").style.display = "none";
 };
+
+function showMeFiveTwo() {
+    document.getElementById("viewfivetwo").style.display = "block";
+    document.getElementById("lightboxone").style.display = "block";
+    document.getElementById("dots52").style.display = "block";
+};
+
+function hideMeFiveTwo() {
+    document.getElementById("viewfivetwo").style.display = "none";
+    document.getElementById("lightboxone").style.display = "none";
+};
+
+function showMeFiveThree() {
+    document.getElementById("viewfivethree").style.display = "block";
+    document.getElementById("lightboxone").style.display = "block";
+    // document.getElementById("dots53").style.display = "block";
+};
+
+function hideMeFiveThree() {
+    document.getElementById("viewfivethree").style.display = "none";
+    document.getElementById("lightboxone").style.display = "none";
+    document.getElementById("upbeanstalk").style.display = "none";
+    document.getElementById("dots5").style.display = "none";
+    document.getElementById("dots52").style.display = "none";
+};
+
 
 function showMeSix() {
     document.getElementById("viewsix").style.display = "block";
